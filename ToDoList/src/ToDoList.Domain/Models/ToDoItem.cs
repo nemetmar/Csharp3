89c9c2b5f38e9d8be8bbea213ab108708f54f4b1
nemetmar <nemetmar@gmail.com>
2024-10-23 22:23:52 +0200

    updated controller for DB

--- ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
+++ ToDoList/src/ToDoList.Domain/Models/ToDoItem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,13 @@ namespace ToDoList.Domain.Models
 {
     public class ToDoItem
     {
-        public int ToDoItemId { get; set;}
+        [Key]
+        public int ToDoItemId { get; set; } //ef core looks for filed <id> od <nameiId>
+        [Length(1, 50)]
+        public string Name { get; set; }
+        [StringLength(250)]
+        public string Description { get; set; }
 
-        public string Name { get; set;}
-
-        public string Description { get; set;}
-
-        public bool IsCompleted { get; set;}
+        public bool IsCompleted { get; set; }
     }
 }
--- ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
+++ ToDoList/src/ToDoList.WebApi/Controllers/ToDoItemsController.cs
@@ -4,8 +4,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ToDoList.Domain.DTOs;
 using ToDoList.Domain.Models;
+using ToDoList.Persistence;
 
 namespace ToDoList.WebApi;
 
@@ -15,14 +17,23 @@ public class ToDoItemsController : ControllerBase
 {
     public static List<ToDoItem> items = [];
 
+    private readonly ToDoItemsContext context;
+
+    public ToDoItemsController(ToDoItemsContext context)
+    {
+        this.context = context;
+    }
+
+
+
     [HttpPost]
     public IActionResult Create(ToDoItemCreateRequestDto request)
     {
         var item = request.ToDomain();
         try
         {
-            item.ToDoItemId = items.Count == 0 ? 1 : items.Max(i => i.ToDoItemId) + 1;
-            items.Add(item);
+            context.ToDoItems.Add(item);
+            context.SaveChanges();
         }
         catch (Exception ex)
         {
@@ -36,19 +47,19 @@ public class ToDoItemsController : ControllerBase
     {
         try
         {
-            if (items.Count == 0)
-                throw new Exception("Nic tu není.");
+
+            if (!context.ToDoItems.Any()) return NotFound();
+            
             var dtoItems = new List<ToDoItemGetResponseDto>();
-            foreach (ToDoItem obj in items)
+            foreach (var obj in context.ToDoItems)
             {
                 dtoItems.Add(ToDoItemGetResponseDto.FromDomain(obj));
             }
+
             return Ok(dtoItems);
         }
         catch (Exception ex)
         {
-            if (items.Count == 0)
-                return NotFound();
             return Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
         }
     }
@@ -56,21 +67,17 @@ public class ToDoItemsController : ControllerBase
     [HttpGet("{toDoItemId:int}")]
     public IActionResult ReadById(int toDoItemId)
     {
-        bool itemNotFound = false;
+
         try
         {
-            var item = items.Find(i => i.ToDoItemId == toDoItemId);
-            if (item == null)
-            {
-                itemNotFound = true;
-                throw new Exception("Položka neexistuje.");
-            }
+            var item = context.ToDoItems.Find(toDoItemId);
+
+            if (item == null) return NotFound();
+
             return Ok(ToDoItemGetResponseDto.FromDomain(item));
         }
         catch (Exception ex)
         {
-            if (itemNotFound)
-                return NotFound();
             return Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
         }
     }
@@ -78,54 +85,42 @@ public class ToDoItemsController : ControllerBase
     [HttpPut("{toDoItemId:int}")]
     public IActionResult UpdateById(int toDoItemId, [FromBody] ToDoItemUpdateRequestDto request)
     {
-        bool itemNotFound = false;
-        var item = request.ToDomain();
-        item.ToDoItemId = toDoItemId;
         try
         {
-            var index = items.FindIndex(i => i.ToDoItemId == toDoItemId);
-            if (index != -1)
-            {
-                items[index] = item;
-            }
-            else
-            {
-                itemNotFound = true;
-                throw new Exception("Položka nenalezena.");
-            }
+            var itemToUpdate = context.ToDoItems.Find(toDoItemId);
+            if(itemToUpdate is null) return NotFound();
+
+            itemToUpdate.Name = request.Name;
+            itemToUpdate.Description = request.Description;
+            itemToUpdate.IsCompleted = request.IsCompleted;
+
+            context.SaveChanges();
+
         }
         catch (Exception ex)
         {
-            if (itemNotFound)
-                return NotFound();
             return Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
         }
-        return CreatedAtAction("Create", ToDoItemGetResponseDto.FromDomain(item));
+        return NoContent();
     }
 
     [HttpDelete("{toDoItemId:int}")]
     public IActionResult DeleteById(int toDoItemId)
     {
-        bool itemNotFound = false;
         try
         {
-            var obj = items.Find(i => i.ToDoItemId == toDoItemId);
-            if (obj != null)
-            {
-                items.Remove(obj);
-            }
-            else
-            {
-                itemNotFound = true;
-                throw new Exception("Položka nenalezena.");
-            }
+            var obj = context.ToDoItems.Find(toDoItemId);
+
+            if(obj is null) return NotFound();
+
+            context.ToDoItems.Remove(obj);
+            context.SaveChanges();
+
         }
         catch (Exception ex)
         {
-            if (itemNotFound)
-                return NotFound();
             return Problem(ex.Message, null, StatusCodes.Status500InternalServerError);
         }
-        return Ok();
+        return NoContent();
     }
 }
